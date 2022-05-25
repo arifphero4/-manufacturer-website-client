9e078c9657b659316abb6c3c785fedebf28d9db8
arifphero <ahammadarif232@gmail.com>
2022-05-25 17:33:30 +0600

    authentication done

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import Home from "./Pages/Home/Home";
 import Navbar from "./Pages/Shared/Navbar";
 import Login from "./Pages/Login/Login";
 import Footer from "./Pages/Shared/Footer";
+import SignUp from "./Pages/Login/SignUp";
+import Purchase from "./Pages/Purchase/Purchase";
+import RequireAuth from "./Pages/Login/RequireAuth";
+import Blogs from "./Pages/Blogs/Blogs";
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
         <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route
+          path="purchase"
+          element={
+            <RequireAuth>
+              <Purchase />
+            </RequireAuth>
+          }
+        />
       </Routes>
       <Footer />
     </div>
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,63 +1,112 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import logo from "../../Assets/Icons/logo.png";
+import { signOut } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const Navbar = () => {
+  const [user] = useAuthState(auth);
+
+  const logout = () => {
+    signOut(auth);
+  };
+
   const menuItems = (
     <>
       <li>
-        <Link to="/">Home</Link>{" "}
+        <Link to="/">Home</Link>
       </li>
       <li>
-        <Link to="/">Home</Link>{" "}
+        <Link to="/blogs">Blogs</Link>
       </li>
+
+      {user && (
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      )}
       <li>
-        <Link to="/">Home</Link>{" "}
-      </li>
-      <li>
-        <Link to="/">Home</Link>{" "}
-      </li>
-      <li>
-        <Link to="/login">Login</Link>{" "}
+        {user ? (
+          <>
+            <button className="btn btn-error" onClick={logout}>
+              Sign Out
+            </button>
+            <button className="btn btn-ghost text-amber-900">
+              {user.displayName}
+            </button>{" "}
+          </>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
       </li>
     </>
   );
   return (
-    <div class="navbar bg-base-100 sticky">
-      <div class="navbar-start">
-        <div class="dropdown">
-          <label tabindex="0" class="btn btn-ghost lg:hidden">
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex="0" className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
+              className="h-5 w-5"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M4 6h16M4 12h8m-8 6h16"
               />
             </svg>
           </label>
           <ul
-            tabindex="0"
-            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            tabIndex="0"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
             {menuItems}
           </ul>
         </div>
-        <a class="btn btn-ghost normal-case text-xl">
-          <img className=" h-16" src={logo} alt="" />{" "}
-        </a>
+        <Link to="/">
+          <a href=" " class="btn btn-ghost normal-case text-xl">
+            <img className=" h-16" src={logo} alt="" />{" "}
+          </a>
+        </Link>
       </div>
-      <div class="navbar-center hidden lg:flex">
-        <ul class="menu menu-horizontal p-0">{menuItems}</ul>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">{menuItems}</ul>
+      </div>
+      <div className="navbar-end">
+        <label
+          tabIndex="1"
+          for="dashboard-sidebar"
+          className="btn btn-ghost lg:hidden"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </label>
       </div>
     </div>
   );
 };
 
 export default Navbar;
+
+/* 
+
+
+*/
