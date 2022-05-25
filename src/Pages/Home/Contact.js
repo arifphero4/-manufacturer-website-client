7a557f3ee0d554fd15eca938a54ff7a82ce39697
arifphero <ahammadarif232@gmail.com>
2022-05-25 16:38:05 +0600

    database connected

--- src/Pages/Home/Contact.js
+++ src/Pages/Home/Contact.js
@@ -11,9 +11,7 @@ const Contact = () => {
       className="bg-primary px-10 py-14 "
     >
       <div className="text-center pb-14 text-white">
-        <p className="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary">
-          Contact Us
-        </p>
+        <p className="text-xl font-bold   text-red-400 ">Contact Us</p>
         <h1 className="text-4xl">Stay connected with us</h1>
       </div>
       <div className="grid grid-cols-1 justify-items-center gap-5">
--- src/Pages/Home/Tools.js
+++ src/Pages/Home/Tools.js
@@ -5,15 +5,13 @@ const Tools = () => {
   const [tools, setTools] = useState();
 
   useEffect(() => {
-    fetch("./products.json")
+    fetch("http://localhost:5000/tool")
       .then((res) => res.json())
       .then((data) => setTools(data));
   }, []);
   return (
-    <div>
-      <h3 text-xl text-center>
-        Our Tools
-      </h3>
+    <div className="my-12">
+      <h3 className="text-2xl text-center my-12 font-bold">Our Tools</h3>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mx-auto">
         {tools?.map((tool) => (
           <ToolsCard key={tool._id} tool={tool}></ToolsCard>
